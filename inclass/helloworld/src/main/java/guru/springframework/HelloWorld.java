70a822aa36e4d84fdadfb7227d72ee95770cda76
pashmash372 <abhilashsivasanker@gmail.com>
2023-07-23 16:16:27 +0530

    completed section5:maven basics

--- inclass/helloworld/src/main/java/guru/springframework/HelloWorld.java
+++ inclass/helloworld/src/main/java/guru/springframework/HelloWorld.java
@@ -1,7 +1,10 @@
 package guru.springframework;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
